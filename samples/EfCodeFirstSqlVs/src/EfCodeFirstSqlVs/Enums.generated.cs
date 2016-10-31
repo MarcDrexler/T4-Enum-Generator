3f6bcb85c42f8c4c9931fec880e66f7d7a789be8
Marc Drexler <marc.drexler@cairo.ag>
2016-10-31 10:37:07 +0100

    T4 enum template added

---
+++ samples/EfCodeFirstSqlVs/src/EfCodeFirstSqlVs/Enums.generated.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.CodeDom.Compiler;
+
+namespace EfCodeFirstSqlVs.Enums
+{
+	/// <summary>
+	/// dbo.Parents auto generated enumeration
+	/// </summary>
+	[GeneratedCode("TextTemplatingFileGenerator", "10")]
+
+		public enum ParentsIds
+	{
+
+		Parent1 = 1,
+
+
+		ParentTwo = 2,
+
+
+		Parent3 = 3,
+
+
+		X4parent = 4,
+
+
+		X5Parent = 5,
+
+
+		Parent6specialss = 6,
+
+
+		Parent7specialSs = 7
+  }
+}
