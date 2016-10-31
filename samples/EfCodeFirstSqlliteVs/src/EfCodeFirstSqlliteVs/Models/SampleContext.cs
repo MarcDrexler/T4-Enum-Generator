f6d30951ac0a5ba6af4933e8a1909db36c37a539
Daniel Sirz <daniel.sirz@gmail.com>
2016-10-31 09:21:19 +0100

    Initial Sample with Visual Studio 2015

---
+++ samples/EfCodeFirstSqlliteVs/src/EfCodeFirstSqlliteVs/Models/SampleContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace EfCodeFirstSqlliteVs.Models
+{
+    public class SampleContext : DbContext
+    {
+        public DbSet<Child> Children { get; set; }
+        public DbSet<Parent> Parents { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Filename=./T4EnumGenerator.EfCodeFirstSqlliteVs.SampleDb.db");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            // set unique indexes
+            modelBuilder.Entity<Parent>()
+                .HasIndex(b => b.Name)
+                .IsUnique();
+        }
+    }
+}
---
+++ samples/EfCodeFirstSqlliteVs/src/EfCodeFirstSqlliteVs/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EfCodeFirstSqlliteVs.Models;
+
+namespace EfCodeFirstSqlliteVs
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            using (var db = new SampleContext())
+            {
+                var parents = db.Parents;
+
+                foreach (var parent in parents)
+                {
+                    System.Console.WriteLine($"{parent.Id} - {parent.Name}");
+                }
+            }
+        }
+    }
+}
